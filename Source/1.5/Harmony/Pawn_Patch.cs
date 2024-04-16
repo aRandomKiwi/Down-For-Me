8b0b0033a29d2b0c7b66907b3aea1a64821bc93f
aRandomKiwi <15568941+aRandomKiwi@users.noreply.github.com>
2024-04-16 07:35:29 +0200

    Add Rimworld 1.5 support

---
+++ Source/1.5/Harmony/Pawn_Patch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace aRandomKiwi.DFM
+{
+    internal static class Pawn_Patch
+    {
+        [HarmonyPatch(typeof(Pawn), "DropAndForbidEverything")]
+        public class DropAndForbidEverything
+        {
+            [HarmonyPrefix]
+            public static bool Listener(Pawn __instance, bool keepInventoryAndEquipmentIfInBed = false)
+            {
+                if (__instance == null || __instance.health.hediffSet.GetFirstHediffOfDef(DefDatabase<HediffDef>.GetNamed("aRandomKiwi_DFM_WTBD")) != null)
+                    return false;
+                return true;
+            }
+        }
+    }
+}
---
+++ Source/1.5/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace aRandomKiwi.DFM
+{
+    [StaticConstructorOnStartup]
+    static class Utils
+    {
+        public static string TranslateTicksToTextIRLSeconds(int ticks)
+        {
+            //Si moins d'une heure ingame alors affichage secondes
+            if (ticks < 2500)
+                return ticks.ToStringSecondsFromTicks();
+            else
+                return ticks.ToStringTicksToPeriodVerbose(true);
+        }
+
+        public static readonly Texture2D texDown = ContentFinder<Texture2D>.Get("UI/Icons/Down", true);
+        public static readonly Texture2D texUndown = ContentFinder<Texture2D>.Get("UI/Icons/Undown", true);
+    }
+}
